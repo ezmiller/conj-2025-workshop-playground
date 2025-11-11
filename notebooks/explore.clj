23f0e499926df4480a9f25e1dff0ccd28be81a89
Ethan Miller <ethanzanemiller@gmail.com>
2025-11-11 08:12:38 -0500

    Add few helpers to explore

---
+++ notebooks/explore.clj
@@ -0,0 +1,32 @@
+(ns explore
+  (:require [tablecloth.api :as tc]
+            [scicloj.tableplot.v1.plotly :as plotly]
+            [java-time :as jt]))
+
+;;; Import the data ;;;
+
+(def workshop-data
+  (tc/dataset "data/clt-311-workshop.csv"
+              {:key-fn keyword}))
+
+;;; Some Helpers ;;;
+
+(def src-fmt
+  (jt/formatter "yyyy/MM/dd HH:mm:ssX"))
+
+(defn ->first-day-of_month [date-str]
+  (->> date-str
+       (jt/local-date src-fmt)
+       (#(jt/adjust % :first-day-of-month))))
+
+(def top-five-requests
+  (-> workshop-data
+      (tc/group-by :REQUEST_TYPE)
+      (tc/aggregate {:count tc/row-count})
+      (tc/order-by :count :desc)
+      (tc/head 5)
+      :$group-name
+      set))
+
+;;; End Helpers ;;;
+
